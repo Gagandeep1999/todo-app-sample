061939de2525f5762fdb17ed65a73b6637bf58ba
gaganevomorf2019 <gagan@evomorf.com>
2020-03-31 17:30:34 +0530

    Updates

---
+++ precache-manifest.4ae790a258ca85c5c333ba6452417c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8dda84adc7fb2a745a185ce4c4642a",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "28400dd59c388ba6b6c0",
+    "url": "/todo-app/static/css/main.6e1ee6ea.chunk.css"
+  },
+  {
+    "revision": "8cd748c23f974f5ff19d",
+    "url": "/todo-app/static/js/2.2824339f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.2824339f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28400dd59c388ba6b6c0",
+    "url": "/todo-app/static/js/main.44c9d945.chunk.js"
+  },
+  {
+    "revision": "8e13b2332a53ed46a08e",
+    "url": "/todo-app/static/js/runtime-main.594827e5.js"
+  }
+]);
